65aa69e54eb6a90760d9fb5297fdd09ac73cdc14
Gabriel Aguilera <gabrieag@umich.edu>
2014-03-04 04:05:20 -0500

    Initial commit

--- Funky Unit Converter/GAViewController.m
+++ Funky Unit Converter/GAViewController.m
@@ -18,12 +18,38 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    //NSLog(@"hello world");
+    int x = 5;
+    int y = 20;
+    int z = -2;
+    
+    //NSLog(@"The value of x is: %i", x);
+    
+    int additionAnswer = x + y;
+    int multiplicationAnswer = x * z;
+    
+    
+    float heightOfEverestBaseCamp = 16900.3;
+    float heightOfEverest = 29029;
+    
+    float distanceToTravel = heightOfEverest - heightOfEverestBaseCamp;
+    distanceToTravel = distanceToTravel - 1000;
+
+    //NSLog(<#NSString *format, ...#>)
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    NSLog(@"didRecieveMemoryWarning method is evaluating.");
 }
 
+- (IBAction)convertUnits:(UIButton *)sender {
+    //Left side calls the floatValue method of String to convert the type.
+    float numberOfBills = [self.numberOfBillsTextField.text floatValue];
+    float numberOfFootballFields = numberOfBills / 91440;
+    self.numberOfBillsLabel.text = [NSString stringWithFormat:@"%f",numberOfFootballFields];
+}
 @end
